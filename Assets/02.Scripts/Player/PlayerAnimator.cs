fa81384fd7fe04a37c7bbc6c36efab3246dd0b78
g1-1g <111170307+g1-1g@users.noreply.github.com>
2026-02-24 14:35:54 +0900

    Feat : Reaction Animation

--- Assets/02.Scripts/Player/PlayerAnimator.cs
+++ Assets/02.Scripts/Player/PlayerAnimator.cs
@@ -8,6 +8,8 @@ public class PlayerAnimator : MonoBehaviour
     private readonly int _speedRatioHash = Animator.StringToHash("Blend");
     private readonly int _attackTriggerHash = Animator.StringToHash("Attack");
     private readonly int _attackTypeHash = Animator.StringToHash("AttackType");
+    private readonly int _reactionTriggerHash = Animator.StringToHash("Reaction");
+    private readonly int _reactionTypeHash = Animator.StringToHash("ReactionType");
 
     private void Awake()
     {
@@ -24,9 +26,35 @@ public class PlayerAnimator : MonoBehaviour
         _animator.SetTrigger(_attackTriggerHash);
     }
 
+    public void SetReactionTrigger()
+    {
+        _animator.SetTrigger(_reactionTriggerHash);
+    }
+
+    public void PlayReaction(int type)
+    {
+        _animator.SetInteger(_reactionTypeHash, type);
+        SetReactionTrigger();
+    }
+
     public void PlayAttack(EAttackType type)
     {
         _animator.SetInteger(_attackTypeHash, (int)type);
         SetAttackTrigger();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)){
+            PlayReaction(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PlayReaction(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PlayReaction(2);
+        }
     }
 }
